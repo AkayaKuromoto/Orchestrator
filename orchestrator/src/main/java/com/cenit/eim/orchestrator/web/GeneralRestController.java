2353ce0260bfbff767d25d22f98e96d668449f92
Pascal Theml <pascal@familie-theml.de>
2021-09-27 19:51:11 +0200

    removed unused import

--- orchestrator/src/main/java/com/cenit/eim/orchestrator/web/GeneralRestController.java
+++ orchestrator/src/main/java/com/cenit/eim/orchestrator/web/GeneralRestController.java
@@ -21,7 +21,6 @@ import org.springframework.web.context.request.NativeWebRequest;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Controller
